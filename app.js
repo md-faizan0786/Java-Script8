71746ec6fca8d94f7a9bd18bba76aa0edb58ff9c
Md Faizan <mdfaizankurisaray@gmail.com>
2025-04-18 20:40:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let links = document.querySelectorAll(".box a");
+for (link of links) {
+    link.style.color = "purple";
+}
+
+// for (let i = 0; i< links.length; i++) {
+//     links[i].style.color = "green";
+// }
