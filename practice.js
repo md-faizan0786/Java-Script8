a31032fe18b3b9560353be2da6298845e4e4d7cf
Md Faizan <mdfaizankurisaray@gmail.com>
2025-04-19 21:30:24 +0530

    2nd commit

---
+++ practice.js
@@ -0,0 +1,25 @@
+//Ans 1
+let button = document.createElement("button");
+let input = document.createElement("input");
+button.innerText="Click me";
+document.querySelector("body").append(input);
+document.querySelector("body").append(button);
+
+
+//Ans 2
+button.setAttribute("id", "btn");
+input.setAttribute("placeholder", "username");
+
+//Ans 3
+let btn = document.querySelector("#btn");
+btn.classList.add("btnStyle");
+
+//Ans 4
+let h1 = document.querySelector("h1");
+h1.innerHTML = "<u>DOM Practice</u>";
+document.querySelector("body").append(h1);
+
+//Ans 5
+let p = document.createElement("p");
+p.innerHTML = "Apna College <b>Delta</b> practice";
+document.querySelector("body").append(p);
